34b321cf78025be3f49738ed7231c6c3480513eb
Mary Welsh <maryrosewelsh42@gmail.com>
2025-07-30 11:06:34 -0600

    Fix fact table joins and enable full dataset processing
    - Improve fct_wikipedia_pageviews to properly join with dim_wikipedia_page instead of regenerating surrogate key
    - Remove LIMIT 1000 from staging model to process full dataset instead of test subset
    - Better data model structure with proper foreign key relationships
    
    These changes improve data quality and enable processing of complete Wikipedia pageview datasets.

--- dbt/models/marts/fct_wikipedia_pageviews.sql
+++ dbt/models/marts/fct_wikipedia_pageviews.sql
@@ -3,11 +3,11 @@
 SELECT
 pageview_date AS pageview_date_id
 , pageview_hour AS pageview_hour_id
-, {{ dbt_utils.generate_surrogate_key(
-    ['pageview_source',
-    'PAGE_TITLE',
-    'PAGE_CATEGORY'
-]) }} AS dim_wikipedia_page_id
+, dwp.dim_wikipedia_page_id
 , VIEW_COUNT
 , BYTE_SIZE
-FROM {{ ref('stg_wikipedia_pageviews') }}
+FROM {{ ref('stg_wikipedia_pageviews') }} stg
+LEFT JOIN {{ ref('dim_wikipedia_page') }} dwp ON stg.pageview_source = dwp.pageview_source
+    AND stg.page_language = dwp.page_language
+    AND stg.page_title = dwp.page_title
+    AND stg.page_category = dwp.page_category
